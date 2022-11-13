798fed53240306f23411217cbd404f9e2e7b1a2f
他知 ITRSS.Net <55280666+itrssnet@users.noreply.github.com>
2022-11-13 18:27:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b16ced559e3c4034a26204114bd41251',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '随意分享 - Share the World',
+  domain: '311322.xyz',
+  author: 'kooShare',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '分享世界，Share the world！',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
